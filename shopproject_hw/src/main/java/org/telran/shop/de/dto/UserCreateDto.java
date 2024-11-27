a896f862cf106705e67788cdff77224456ddd81a
tolya <tolya@gmx.de>
2024-11-27 19:42:03 +0100

    добавлен класс UserCreateDto в пакете dto

---
+++ shopproject_hw/src/main/java/org/telran/shop/de/dto/UserCreateDto.java
@@ -0,0 +1,16 @@
+package org.telran.shop.de.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserCreateDto {
+
+    private String login;
+
+    private String email;
+
+}
